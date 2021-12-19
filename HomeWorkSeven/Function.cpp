aaf61ce8bfe0c0d9a344d726b935ea0fcb2f9ad2
Teribble <ggameparadox@gmail.com>
2021-12-20 00:46:06 +0300

    Не готова только сторая функция

--- HomeWorkSeven/Function.cpp
+++ HomeWorkSeven/Function.cpp
@@ -1,37 +1,74 @@
 #include "Function.h"
 
-void compressText( const std::string text )
+std::string compressText( const std::string text )
 {
+    std::string bufferString;
+
+    // Булиевая переменная первого вохождения символа пробела
+    bool firstSpaces = true;
+
+     // Булиевая переменная символа пробела
+    bool space = false;
+    
+    // Цикл до конца строки
+    for(int i = 0; text[i] != '\0'; i++)
+    {
+        if(text[ i ] == ' ' && true == firstSpaces)
+        {
+            bufferString += text[ i ];
+        }
+        else
+        {
+            firstSpaces = false;
+
+            if(text[ i ] != ' ')
+            {
+                bufferString += text[ i ];
+                space = false;
+            }
+            else if(text[ i ] == ' ' && false == space)
+            {
+                bufferString += text[ i ];
+                space = true;
+            }
+            else if(text[ i ] == ' ' && true == space)
+            {
+                continue;
+            }
+            
+        }
+    }
+
+    return bufferString;
 }
 
 void deleteComment( const std::string text )
 {
+   
 }
 
-std::string searchAndHighlight( const std::string text, const std::string chars )
+std::string searchAndHighlight( std::string text, const std::string chars )
 {
-	bool flag = false;
-	std::string buffer;
-	for(int i = 0; text[i] != '\0'; i++)
-	{
-		if(text[ i ] == chars[ 0 ])
-		{
-			for(int j = 0, k = i; chars[j] != '\0'; j++,k++)
-			{
+    // Бефферная строка
+    std::string s = "";
 
-				flag = false;
+    // Счетчик
+    int i;
 
-				if(text[ k ] == chars[ j ])
-				{
-					flag = true;
-				}
-				else
-				{
-					break;
-				}
-			}
-		}
-	}
+    // Цикл, пока метод find не вернет -1, решил попробовать не через цикл for
+    while(( i = text.find( chars ) ) >= 0)
+    {
+        // в буффер записываем символы, пока не встретится наша подстрока, как только на встретится, добавляем в нее
+        // при помощи конкатинации скобки и подстроку
+        s += text.substr( 0 , i ) + "(" + chars + ")";
 
-	return 0;
+        // перезаписываем основную строку, от длины первого вхождения + длина подстроки и до конца
+        text = text.substr( i + chars.length() );
+    }
+
+    // буфферу присваиваем остаток строки
+    s += text;
+
+    //выводим измененную строку
+    return s;
 }
--- HomeWorkSeven/Function.h
+++ HomeWorkSeven/Function.h
@@ -2,15 +2,10 @@
 #include <iostream>
 
 // Функция сжатия текста
-void compressText( const std::string text );
+std::string compressText( const std::string text );
 
 // Функция котороя удаляет комментарии из текста, если они есть
 void deleteComment( const std::string text );
 
 // Функция, которая ищет вхождение подстроки в строку и выделяет ее скобками
-std::string searchandРighlight( const char* text );
-
-
-
-
-int myLength( const char* text );
+std::string searchAndHighlight( std::string text , const std::string chars );
